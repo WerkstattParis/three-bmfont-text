6c90793e2cf5d636e904d118b8c438f51f007921
Workstation <lateek35@gmail.com>
2020-03-19 11:48:49 +0100

    remove createOrbitViewer and old things

--- index.js
+++ index.js
@@ -78,37 +78,37 @@ TextGeometry.prototype.update = function (opt) {
   this.deleteAttribute('page');
 }
 
-TextGeometry.prototype.computeBoundingSphere = function () {
-  if (this.boundingSphere === null) {
-    this.boundingSphere = new THREE.Sphere()
-  }
+// TextGeometry.prototype.computeBoundingSphere = function () {
+//   if (this.boundingSphere === null) {
+//     this.boundingSphere = new THREE.Sphere()
+//   }
 
-  var positions = this.attributes.position.array
-  var itemSize = this.attributes.position.itemSize
-  if (!positions || !itemSize || positions.length < 2) {
-    this.boundingSphere.radius = 0
-    this.boundingSphere.center.set(0, 0, 0)
-    return
-  }
-  utils.computeSphere(positions, this.boundingSphere)
-  if (isNaN(this.boundingSphere.radius)) {
-    console.error('THREE.BufferGeometry.computeBoundingSphere(): ' +
-      'Computed radius is NaN. The ' +
-      '"position" attribute is likely to have NaN values.')
-  }
-}
+//   var positions = this.attributes.position.array
+//   var itemSize = this.attributes.position.itemSize
+//   if (!positions || !itemSize || positions.length < 2) {
+//     this.boundingSphere.radius = 0
+//     this.boundingSphere.center.set(0, 0, 0)
+//     return
+//   }
+//   utils.computeSphere(positions, this.boundingSphere)
+//   if (isNaN(this.boundingSphere.radius)) {
+//     console.error('THREE.BufferGeometry.computeBoundingSphere(): ' +
+//       'Computed radius is NaN. The ' +
+//       '"position" attribute is likely to have NaN values.')
+//   }
+// }
 
-TextGeometry.prototype.computeBoundingBox = function () {
-  if (this.boundingBox === null) {
-    this.boundingBox = new THREE.Box3()
-  }
+// TextGeometry.prototype.computeBoundingBox = function () {
+//   if (this.boundingBox === null) {
+//     this.boundingBox = new THREE.Box3()
+//   }
 
-  var bbox = this.boundingBox
-  var positions = this.attributes.position.array
-  var itemSize = this.attributes.position.itemSize
-  if (!positions || !itemSize || positions.length < 2) {
-    bbox.makeEmpty()
-    return
-  }
-  utils.computeBox(positions, bbox)
-}
+//   var bbox = this.boundingBox
+//   var positions = this.attributes.position.array
+//   var itemSize = this.attributes.position.itemSize
+//   if (!positions || !itemSize || positions.length < 2) {
+//     bbox.makeEmpty()
+//     return
+//   }
+//   utils.computeBox(positions, bbox)
+// }
--- test/test-msdf.js
+++ test/test-msdf.js
@@ -1,88 +1,81 @@
-/*
-  This is an example of 2D rendering, simply
-  using bitmap fonts in orthographic space.
-
-  var geom = createText({
-    multipage: true,
-    ... other options
-  })
- */
-
 global.THREE = require('three')
-var createOrbitViewer = require('three-orbit-viewer')(THREE)
-var shuffle = require('array-shuffle')
-var quotes = shuffle(require('sun-tzu-quotes/quotes.json').join(' ').split('.'))
 var createText = require('../')
 var MSDFShader = require('../shaders/msdf')
-var palettes = require('nice-color-palettes')
-var palette = palettes[5]
-var background = palette.shift()
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2(9999, 9999);
+let scene, camera, renderer, text, cube;
+
 
 require('./load')({
-  font: 'fnt/Roboto-msdf.json',
-  image: 'fnt/Roboto-msdf.png'
+    font: 'fnt/Roboto-msdf.json',
+    image: 'fnt/Roboto-msdf.png'
 }, start)
 
-function start (font, texture) {
-  var app = createOrbitViewer({
-    clearColor: background,
-    clearAlpha: 1.0,
-    fov: 65,
-    position: new THREE.Vector3()
-  })
+function start(font, texture) {
+    camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / -2, window.innerHeight / 2, -1000, 1000);
 
-  app.camera = new THREE.OrthographicCamera()
-  app.camera.left = 0
-  app.camera.top = 0
-  app.camera.near = -1
-  app.camera.far = 1000
+    var canvas = document.getElementById("canvas");
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        scene: scene,
+        antialias: true
+    });
 
-  var container = new THREE.Object3D()
-  app.scene.add(container)
-  var count = 25
-  for (var i = 0; i < count; i++) {
+    renderer.setAnimationLoop(update.bind(this));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.innerWidth / window.innerHeight);
+
+
+
     createGlyph()
-  }
 
-  var time = 0
-  // update orthographic
-  app.on('tick', function (dt) {
-    time += dt / 1000
-    var s = (Math.sin(time * 0.5) * 0.5 + 0.5) * 2.0 + 0.5
-    container.scale.set(s, s, s)
-    // update camera
-    var width = app.engine.width
-    var height = app.engine.height
-    app.camera.left = -width / 2
-    app.camera.right = width / 2
-    app.camera.top = -height / 2
-    app.camera.bottom = height / 2
-    app.camera.updateProjectionMatrix()
-  })
+    var geometry = new THREE.BoxGeometry(100, 100, 100);
+    var material = new THREE.MeshBasicMaterial({ color: 0xf00000 });
+    cube = new THREE.Mesh(geometry, material);
 
-  function createGlyph () {
-    var angle = (Math.random() * 2 - 1) * Math.PI
-    var geom = createText({
-      text: quotes[Math.floor(Math.random() * quotes.length)].split(/\s+/g).slice(0, 6).join(' '),
-      font: font,
-      align: 'left',
-      flipY: texture.flipY
-    })
+    camera.position.z = 100;
+    scene.add(cube);
 
-    var material = new THREE.RawShaderMaterial(MSDFShader({
-      map: texture,
-      transparent: true,
-      color: palette[Math.floor(Math.random() * palette.length)]
-    }))
+    window.addEventListener('mousemove', onMouseMove, false);
 
-    var layout = geom.layout
-    var text = new THREE.Mesh(geom, material)
-    text.position.set(0, -layout.descender + layout.height, 0)
-    text.scale.multiplyScalar(Math.random() * 0.5 + 0.5)
+    function createGlyph() {
+        var geom = createText({
+            font: font,
+            text: `SAMPLE TEXT`,
+            // lineHeight: 100
+        })
 
-    var textAnchor = new THREE.Object3D()
-    textAnchor.add(text)
-    textAnchor.rotation.z = angle
-    container.add(textAnchor)
-  }
+        var material = new THREE.RawShaderMaterial(MSDFShader({
+            map: texture,
+            color: 0xfffffff,
+            side: THREE.DoubleSide,
+            transparent: true,
+            negate: true
+        }))
+
+        text = new THREE.Mesh(geom, material)
+
+
+        scene.add(text)
+    }
+}
+
+
+
+function update() {
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(scene.children);
+    
+    for (let i = 0; i < intersects.length; i++) {
+        intersects[i].object.material.wireframe = true;
+    }
+
+    renderer.render(scene, camera);
+};
+
+function onMouseMove(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 }
