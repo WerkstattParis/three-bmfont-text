f8cd75c01acdd1c12e02c119036f97eb0e3e3195
Thomas Lacroix <t.f.m.lacroix@gmail.com>
2020-03-31 15:13:46 +0200

    :fire: added transitions WIP

---
+++ Dillinger_Transition/links.js
@@ -0,0 +1,167 @@
+const PATH = `fnt/transitions`
+
+FontTransitions.addFontTransition('Bold_to_Light',
+    [{
+            json: `${PATH}/Bold_to_Light/300.json`,
+            texture: `${PATH}/Bold_to_Light/300.png`
+        },
+        {
+            json: `${PATH}/Bold_to_Light/500.json`,
+            texture: `${PATH}/Bold_to_Light/500.png`
+        },
+        {
+            json: `${PATH}/Bold_to_Light/700.json`,
+            texture: `${PATH}/Bold_to_Light/700.png`
+        },
+        {
+            json: `${PATH}/Bold_to_Light/900.json`,
+            texture: `${PATH}/Bold_to_Light/900.png`
+        },
+    ],
+    onTransitionLoaded
+);
+
+FontTransitions.addFontTransition('BoldWide_to_LightCondensed',
+    [{
+            json: `${PATH}/BoldWide_to_LightCondensed/100_900.json`,
+            texture: `${PATH}/BoldWide_to_LightCondensed/100_900.png`
+        },
+        {
+            json: `${PATH}/BoldWide_to_LightCondensed/75_700.json`,
+            texture: `${PATH}/BoldWide_to_LightCondensed/75_700.png`
+        },
+        {
+            json: `${PATH}/BoldWide_to_LightCondensed/50_500.json`,
+            texture: `${PATH}/BoldWide_to_LightCondensed/50_500.png`
+        },
+        {
+            json: `${PATH}/BoldWide_to_LightCondensed/25_300.json`,
+            texture: `${PATH}/BoldWide_to_LightCondensed/25_300.png`
+        },
+        {
+            json: `${PATH}/BoldWide_to_LightCondensed/0_100.json`,
+            texture: `${PATH}/BoldWide_to_LightCondensed/0_100.png`
+        },
+    ],
+    onTransitionLoaded
+);
+
+FontTransitions.addFontTransition('LightWide_to_ThinCondensed',
+    [{
+            json: `${PATH}/LightWide_to_ThinCondensed/300_40.json`,
+            texture: `${PATH}/LightWide_to_ThinCondensed/300_40.png`
+        },
+        {
+            json: `${PATH}/LightWide_to_ThinCondensed/220_20.json`,
+            texture: `${PATH}/LightWide_to_ThinCondensed/220_20.png`
+        },
+        {
+            json: `${PATH}/LightWide_to_ThinCondensed/150_1.json`,
+            texture: `${PATH}/LightWide_to_ThinCondensed/150_1.png`
+        },
+    ],
+    onTransitionLoaded
+);
+
+FontTransitions.addFontTransition('Wide_to_Condensed',
+    [{
+            json: `${PATH}/Wide_to_Condensed/900_100.json`,
+            texture: `${PATH}/Wide_to_Condensed/900_100.png`
+        },
+        {
+            json: `${PATH}/Wide_to_Condensed/900_60.json`,
+            texture: `${PATH}/Wide_to_Condensed/900_60.png`
+        },
+        {
+            json: `${PATH}/Wide_to_Condensed/900_30.json`,
+            texture: `${PATH}/Wide_to_Condensed/900_30.png`
+        },
+        {
+            json: `${PATH}/Wide_to_Condensed/900_1.json`,
+            texture: `${PATH}/Wide_to_Condensed/900_1.png`
+        },
+    ],
+    onTransitionLoaded
+);
+
+FontTransitions.addFontTransition('BoldNormal_to_ThinCondensed',
+    [{
+            json: `${PATH}/BoldNormal_to_ThinCondensed/40_900.json`,
+            texture: `${PATH}/BoldNormal_to_ThinCondensed/40_900.png`
+        },
+        {
+            json: `${PATH}/BoldNormal_to_ThinCondensed/30_600.json`,
+            texture: `${PATH}/BoldNormal_to_ThinCondensed/30_600.png`
+        },
+        {
+            json: `${PATH}/BoldNormal_to_ThinCondensed/10_300.json`,
+            texture: `${PATH}/BoldNormal_to_ThinCondensed/10_300.png`
+        },
+        {
+            json: `${PATH}/BoldNormal_to_ThinCondensed/1_100.json`,
+            texture: `${PATH}/BoldNormal_to_ThinCondensed/1_100.png`
+        },
+    ],
+    onTransitionLoaded
+);
+
+FontTransitions.addFontTransition('Bold_to_Thin',
+    [{
+            json: `${PATH}/Bold_to_Thin/900_60.json`,
+            texture: `${PATH}/Bold_to_Thin/900_60.png`
+        },
+        {
+            json: `${PATH}/Bold_to_Thin/600_60.json`,
+            texture: `${PATH}/Bold_to_Thin/600_60.png`
+        },
+        {
+            json: `${PATH}/Bold_to_Thin/300_60.json`,
+            texture: `${PATH}/Bold_to_Thin/300_60.png`
+        },
+        {
+            json: `${PATH}/Bold_to_Thin/1_60.json`,
+            texture: `${PATH}/Bold_to_Thin/1_60.png`
+        },
+    ],
+    onTransitionLoaded
+);
+
+
+
+FontTransitions.addFontTransition('BoldNormal_to_BoldCondensed',
+    [{
+            json: `${PATH}/BoldNormal_to_BoldCondensed/900_60.json`,
+            texture: `${PATH}/BoldNormal_to_BoldCondensed/900_60.png`
+        },
+        {
+            json: `${PATH}/BoldNormal_to_BoldCondensed/900_30.json`,
+            texture: `${PATH}/BoldNormal_to_BoldCondensed/900_30.png`
+        },
+        {
+            json: `${PATH}/BoldNormal_to_BoldCondensed/900_1.json`,
+            texture: `${PATH}/BoldNormal_to_BoldCondensed/900_1.png`
+        },
+    ],
+    onTransitionLoaded
+);
+
+FontTransitions.addFontTransition('ThinCondensed_to_BoldCondensed',
+    [{
+            json: `${PATH}/ThinCondensed_to_BoldCondensed/900_1.json`,
+            texture: `${PATH}/ThinCondensed_to_BoldCondensed/900_1.png`
+        },
+        {
+            json: `${PATH}/ThinCondensed_to_BoldCondensed/600_1.json`,
+            texture: `${PATH}/ThinCondensed_to_BoldCondensed/600_1.png`
+        },
+        {
+            json: `${PATH}/ThinCondensed_to_BoldCondensed/300_1.json`,
+            texture: `${PATH}/ThinCondensed_to_BoldCondensed/300_1.png`
+        },
+        {
+            json: `${PATH}/ThinCondensed_to_BoldCondensed/100_1.json`,
+            texture: `${PATH}/ThinCondensed_to_BoldCondensed/100_1.png`
+        },
+    ],
+    onTransitionLoaded
+);
--- test/test-msdf.js
+++ test/test-msdf.js
@@ -14,11 +14,29 @@ const textArray = [
     `DIRECTORS`,
     `ABOUT`,
     `NEWS`,
-    `ALLAN`,
-    `RAQUIN`,
-    `THREE BM FONT`,
-    `VARIABLE FONT`,
-    `THREEJS`
+    `DIRECTORS`,
+    `ABOUT`,
+    `NEWS`,
+    `DIRECTORS`,
+    `ABOUT`,
+    `NEWS`,
+    `DIRECTORS`,
+    `ABOUT`,
+    `NEWS`,
+    `DIRECTORS`,
+    `ABOUT`,
+    `NEWS`,
+]
+
+const transitions = [
+    `Bold_to_Light`,
+    `BoldWide_to_LightCondensed`,
+    `LightWide_to_ThinCondensed`,
+    `Wide_to_Condensed`,
+    `BoldNormal_to_ThinCondensed`,
+    `Bold_to_Thin`,
+    `BoldNormal_to_BoldCondensed`,
+    `ThinCondensed_to_BoldCondensed`,
 ]
 
 start();
@@ -40,10 +58,18 @@ function start() {
 
     FontTransitions.setRendererPreload(renderer);
     FontTransitions.addFontTransition('thin2bold',
-        [
-            { json: "fnt/werk/4/1.json", texture: "fnt/werk/4/1.png" },
-            { json: "fnt/werk/4/2.json", texture: "fnt/werk/4/2.png" },
-            { json: "fnt/werk/4/3.json", texture: "fnt/werk/4/3.png" },
+        [{
+                json: "fnt/werk/4/1.json",
+                texture: "fnt/werk/4/1.png"
+            },
+            {
+                json: "fnt/werk/4/2.json",
+                texture: "fnt/werk/4/2.png"
+            },
+            {
+                json: "fnt/werk/4/3.json",
+                texture: "fnt/werk/4/3.png"
+            },
             // { json: "fnt/werk/4/4.json", texture: "fnt/werk/4/4.png" },
             // { json: "fnt/werk/4/5.json", texture: "fnt/werk/4/5.png" },
             // { json: "fnt/werk/4/6.json", texture: "fnt/werk/4/6.png" }
@@ -55,49 +81,258 @@ function start() {
             // { json: "fnt/werk/4/1.json", texture: "fnt/werk/4/1.png" },
             // { json: "fnt/werk/4/2.json", texture: "fnt/werk/4/2.png" },
             // { json: "fnt/werk/4/3.json", texture: "fnt/werk/4/3.png" },
-            { json: "fnt/werk/4/4.json", texture: "fnt/werk/4/4.png" },
-            { json: "fnt/werk/4/5.json", texture: "fnt/werk/4/5.png" },
-            { json: "fnt/werk/4/6.json", texture: "fnt/werk/4/6.png" }
+            {
+                json: "fnt/werk/4/4.json",
+                texture: "fnt/werk/4/4.png"
+            },
+            {
+                json: "fnt/werk/4/5.json",
+                texture: "fnt/werk/4/5.png"
+            },
+            {
+                json: "fnt/werk/4/6.json",
+                texture: "fnt/werk/4/6.png"
+            }
         ],
         onTransitionLoaded
     );
 
+    FontTransitions.addFontTransition('Bold_to_Light',
+        [{
+                json: `fnt/transitions/Bold_to_Light/300.json`,
+                texture: `fnt/transitions/Bold_to_Light/300.png`
+            },
+            {
+                json: `fnt/transitions/Bold_to_Light/500.json`,
+                texture: `fnt/transitions/Bold_to_Light/500.png`
+            },
+            {
+                json: `fnt/transitions/Bold_to_Light/700.json`,
+                texture: `fnt/transitions/Bold_to_Light/700.png`
+            },
+            {
+                json: `fnt/transitions/Bold_to_Light/900.json`,
+                texture: `fnt/transitions/Bold_to_Light/900.png`
+            },
+        ],
+        onTransitionLoaded
+    );
+
+
+    FontTransitions.addFontTransition('BoldWide_to_LightCondensed',
+        [{
+                json: "fnt/transitions/BoldWide_to_LightCondensed/100_900.json",
+                texture: "fnt/transitions/BoldWide_to_LightCondensed/100_900.png"
+            },
+            {
+                json: "fnt/transitions/BoldWide_to_LightCondensed/75_700.json",
+                texture: "fnt/transitions/BoldWide_to_LightCondensed/75_700.png"
+            },
+            {
+                json: "fnt/transitions/BoldWide_to_LightCondensed/50_500.json",
+                texture: "fnt/transitions/BoldWide_to_LightCondensed/50_500.png"
+            },
+            {
+                json: "fnt/transitions/BoldWide_to_LightCondensed/25_300.json",
+                texture: "fnt/transitions/BoldWide_to_LightCondensed/25_300.png"
+            },
+            {
+                json: "fnt/transitions/BoldWide_to_LightCondensed/0_100.json",
+                texture: "fnt/transitions/BoldWide_to_LightCondensed/0_100.png"
+            },
+        ],
+        onTransitionLoaded
+    );
+
+    FontTransitions.addFontTransition('LightWide_to_ThinCondensed',
+        [{
+                json: "fnt/transitions/LightWide_to_ThinCondensed/300_40.json",
+                texture: "fnt/transitions/LightWide_to_ThinCondensed/300_40.png"
+            },
+            {
+                json: "fnt/transitions/LightWide_to_ThinCondensed/220_20.json",
+                texture: "fnt/transitions/LightWide_to_ThinCondensed/220_20.png"
+            },
+            {
+                json: "fnt/transitions/LightWide_to_ThinCondensed/150_1.json",
+                texture: "fnt/transitions/LightWide_to_ThinCondensed/150_1.png"
+            },
+        ],
+        onTransitionLoaded
+    );
+
+    FontTransitions.addFontTransition('Wide_to_Condensed',
+        [{
+                json: "fnt/transitions/Wide_to_Condensed/900_100.json",
+                texture: "fnt/transitions/Wide_to_Condensed/900_100.png"
+            },
+            {
+                json: "fnt/transitions/Wide_to_Condensed/900_60.json",
+                texture: "fnt/transitions/Wide_to_Condensed/900_60.png"
+            },
+            {
+                json: "fnt/transitions/Wide_to_Condensed/900_30.json",
+                texture: "fnt/transitions/Wide_to_Condensed/900_30.png"
+            },
+            {
+                json: "fnt/transitions/Wide_to_Condensed/900_1.json",
+                texture: "fnt/transitions/Wide_to_Condensed/900_1.png"
+            },
+        ],
+        onTransitionLoaded
+    );
+
+    FontTransitions.addFontTransition('BoldNormal_to_ThinCondensed',
+        [{
+                json: "fnt/transitions/BoldNormal_to_ThinCondensed/40_900.json",
+                texture: "fnt/transitions/BoldNormal_to_ThinCondensed/40_900.png"
+            },
+            {
+                json: "fnt/transitions/BoldNormal_to_ThinCondensed/30_600.json",
+                texture: "fnt/transitions/BoldNormal_to_ThinCondensed/30_600.png"
+            },
+            {
+                json: "fnt/transitions/BoldNormal_to_ThinCondensed/10_300.json",
+                texture: "fnt/transitions/BoldNormal_to_ThinCondensed/10_300.png"
+            },
+            {
+                json: "fnt/transitions/BoldNormal_to_ThinCondensed/1_100.json",
+                texture: "fnt/transitions/BoldNormal_to_ThinCondensed/1_100.png"
+            },
+        ],
+        onTransitionLoaded
+    );
+
+    FontTransitions.addFontTransition('Bold_to_Thin',
+        [{
+                json: "fnt/transitions/Bold_to_Thin/900_60.json",
+                texture: "fnt/transitions/Bold_to_Thin/900_60.png"
+            },
+            {
+                json: "fnt/transitions/Bold_to_Thin/600_60.json",
+                texture: "fnt/transitions/Bold_to_Thin/600_60.png"
+            },
+            {
+                json: "fnt/transitions/Bold_to_Thin/300_60.json",
+                texture: "fnt/transitions/Bold_to_Thin/300_60.png"
+            },
+            {
+                json: "fnt/transitions/Bold_to_Thin/1_60.json",
+                texture: "fnt/transitions/Bold_to_Thin/1_60.png"
+            },
+        ],
+        onTransitionLoaded
+    );
+
+
+
+    FontTransitions.addFontTransition('BoldNormal_to_BoldCondensed',
+        [{
+                json: "fnt/transitions/BoldNormal_to_BoldCondensed/900_60.json",
+                texture: "fnt/transitions/BoldNormal_to_BoldCondensed/900_60.png"
+            },
+            {
+                json: "fnt/transitions/BoldNormal_to_BoldCondensed/900_30.json",
+                texture: "fnt/transitions/BoldNormal_to_BoldCondensed/900_30.png"
+            },
+            {
+                json: "fnt/transitions/BoldNormal_to_BoldCondensed/900_1.json",
+                texture: "fnt/transitions/BoldNormal_to_BoldCondensed/900_1.png"
+            },
+        ],
+        onTransitionLoaded
+    );
+
+    FontTransitions.addFontTransition('ThinCondensed_to_BoldCondensed',
+        [{
+                json: "fnt/transitions/ThinCondensed_to_BoldCondensed/900_1.json",
+                texture: "fnt/transitions/ThinCondensed_to_BoldCondensed/900_1.png"
+            },
+            {
+                json: "fnt/transitions/ThinCondensed_to_BoldCondensed/600_1.json",
+                texture: "fnt/transitions/ThinCondensed_to_BoldCondensed/600_1.png"
+            },
+            {
+                json: "fnt/transitions/ThinCondensed_to_BoldCondensed/300_1.json",
+                texture: "fnt/transitions/ThinCondensed_to_BoldCondensed/300_1.png"
+            },
+            {
+                json: "fnt/transitions/ThinCondensed_to_BoldCondensed/100_1.json",
+                texture: "fnt/transitions/ThinCondensed_to_BoldCondensed/100_1.png"
+            },
+        ],
+        onTransitionLoaded
+    );
+
+
     window.addEventListener('mousemove', onMouseMove, false);
 }
-function onTransitionLoaded(){
-    nbrTransitionLoaded++;
-    if( nbrTransitionLoaded < 2) return;
 
-    textArray.forEach((text, index)=>{
+function onTransitionLoaded() {
+    nbrTransitionLoaded++;
+    if (nbrTransitionLoaded < 10) return;
+    /*
+    textArray.forEach((text, index) => {
         let instance = new FontVariableGL(
             text,
-            [FontTransitions.getFontTransition('thin2bold'), FontTransitions.getFontTransition('thin2bold2')],
-            function () { console.log('mouseEnter') },
-            function () { console.log('mouseLeave') },
+            [FontTransitions.getFontTransition('ThinCondensed_to_BoldCondensed')],
+            function () {
+                console.log('mouseEnter')
+            },
+            function () {
+                console.log('mouseLeave')
+            },
         );
         instance.mesh.position.x = -window.innerWidth / 2;
         instance.mesh.position.y = (-window.innerHeight / 2) + (100 * (index + 1));
         instance.mesh.scale.multiplyScalar(1);
         scene.add(instance.mesh);
-        textInstances.push( instance );
+        textInstances.push(instance);
     })
+    */
+
+
+    transitions.forEach((transition, mainIndex) => {
+        textArray.forEach((text, index) => {
+            console.log(transition, mainIndex);
+
+            let instance = new FontVariableGL(
+                text,
+                [FontTransitions.getFontTransition(transition)],
+                function () {
+                    console.log('mouseEnter')
+                },
+                function () {
+                    console.log('mouseLeave')
+                },
+            );
+            //instance.mesh.position.x = -window.innerWidth / 2;
+            //instance.mesh.position.y = (-window.innerHeight / 2) + (20 * (index + 1)) + 300 * mainIndex;
+            instance.mesh.position.x = -window.innerWidth / 2 + Math.random() * 1000;
+            instance.mesh.position.y = (-window.innerHeight / 2) + Math.random() * 1000;
+            instance.mesh.scale.multiplyScalar(0.5);
+            scene.add(instance.mesh);
+            textInstances.push(instance);
+        })
+    })
+
 
     renderer.setAnimationLoop(update.bind(this));
 }
 
 function update() {
-    inc+= .15;
-    textInstances.forEach((textInstance, index)=>{
-        textInstance.setProgress((Math.sin(inc + (index / 1)) + 1) / 2 );
+    inc += .15;
+    textInstances.forEach((textInstance, index) => {
+        textInstance.setProgress((Math.sin(inc + (index / 1)) + 1) / 2);
         // if (textInstancesetP> 0.5 ) { textInstance.currentTransition = textInstance.arrayTansition[1]; }
     })
-    
+
     //RAYCASTER
     raycaster.setFromCamera(mouse, camera);
     for (let index = 0; index < textInstances.length; index++) {
-        if( raycaster.intersectObject(textInstances[index].mesh).length > 0 ){
+        if (raycaster.intersectObject(textInstances[index].mesh).length > 0) {
             textInstances[index].mouseEnter()
-        }else{
+        } else {
             textInstances[index].mouseLeave()
         }
     }
@@ -107,5 +342,5 @@ function update() {
 
 function onMouseMove(event) {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
